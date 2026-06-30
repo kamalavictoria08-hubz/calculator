4c50373488b3a786abf62f102e5513e4f54b365b
victoria <kamalavictoria08@gmail.com>
2026-06-30 07:50:27 +0200

    addition

---
+++ index.js
@@ -0,0 +1,9 @@
+function tori(num) {
+    const toriShow = document.getElementById('calc')
+    toriShow.value += num;
+}
+function toriClear(){
+    const variabo = document.getElementById("calc")
+    variabo.value = '';
+
+}

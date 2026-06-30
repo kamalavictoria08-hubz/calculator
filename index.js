59fe257c9c03d6ffefa67295ce46853e0c840e71
victoria <kamalavictoria08@gmail.com>
2026-06-30 08:40:11 +0200

    next

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function tori(num) {
-    const toriShow = document.getElementById('calc')
+    const toriShow = document.getElementById('calc');
     toriShow.value += num;
 }
 function toriClear(){
-    const variabo = document.getElementById("calc")
+    const variabo = document.getElementById("calc");
     variabo.value = '';
 
+}
+function toriResults(){
+    const variabo = document.getElementById('calc');
+    variabo.value = eval(variabo.value);
+}
+function squareRoot(){
+    const variabo = document.getElementById('calc');
+    variabo.value = Math.sqrt(eval(variabo.value));
 }
